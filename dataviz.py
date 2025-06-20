244b3293a22b7faddf7931986815d2055e5a04b2
SofiaDMurillo <sofiamurillo@gmail.com>
2025-06-20 12:09:34 +0200

    CSV file

--- dataviz.py
+++ dataviz.py
@@ -1,5 +1,11 @@
+import pandas as pd
+
 class DataAnalysis:
 
     def read_csv(self):
         # read the csv file with pandas
-        pass
+
+        base_file = pd.read_csv('./properties06191148_modified.csv')
+        print(base_file.head())
+
+        

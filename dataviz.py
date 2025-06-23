991bd549559bfeb72bae624a7918618c37eaa085
SofiaDMurillo <sofiamurillo@gmail.com>
2025-06-23 12:23:57 +0200

    Garden adn terrace changes done

--- dataviz.py
+++ dataviz.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 class DataAnalysis:
@@ -45,3 +46,22 @@ class DataAnalysis:
         df = pd.read_csv(path)
         print(df.info())
         return df
+
+    # check "hasGarden" column, if "TRUE" change to 1, otherwise make it 0
+    def convert_has_garden (self, df):         
+        df["hasGarden"]= np.where(df["hasGarden"] == True,1,0)
+        return df
+
+    # check "gardenSurface" column, if empty change to 0, leave current value if NOT empty
+    def convert_garden_surface (self, df):
+        df["gardenSurface"]= df["gardenSurface"].fillna(0)
+        return df
+
+    # check "hasTerrace" column, if "TRUE" change to 1 otherwise make it 0
+    def convert_has_terrace (self, df):         
+        df["hasTerrace"]= np.where(df["hasTerrace"] == True,1,0)
+        return df
+
+
+
+
--- main.py
+++ main.py
@@ -2,8 +2,18 @@ from dataviz import DataAnalysis
 
 def main():
     dataviz = DataAnalysis()
-    csv = dataviz.read_csv("./immoweb-dataset.csv")
-    print(csv)
+    df = dataviz.read_csv("./immoweb-dataset.csv")
+    
+    df = dataviz.convert_has_garden(df)
+    print(df.info())
 
+    df = dataviz.convert_garden_surface (df)
+    print(df.info())
+
+    df = dataviz.convert_has_terrace (df)
+    print(df.info())
+
+        
 if __name__ == "__main__":
     main()
+
